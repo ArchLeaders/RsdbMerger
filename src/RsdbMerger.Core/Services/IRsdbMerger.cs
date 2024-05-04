859ed485918372a0f410e90d95ed86d8f430b5db
ArchLeaders <archleadership28@gmail.com>
2024-05-04 16:26:17 -0700

    Impl merger service

---
+++ src/RsdbMerger.Core/Services/IRsdbMerger.cs
@@ -0,0 +1,9 @@
+﻿using RsdbMerger.Core.Models;
+
+namespace RsdbMerger.Core.Services;
+
+public interface IRsdbMerger
+{
+    public void Merge(ArraySegment<byte>[] merge, Stream output);
+    public void CreateChangelog(ReadOnlySpan<char> canonical, ArraySegment<byte> data, RsdbFile target, Stream output);
+}
---
+++ src/RsdbMerger.Core/Services/RsdbMergerService.cs
@@ -0,0 +1,36 @@
+﻿using RsdbMerger.Core.Mergers;
+
+namespace RsdbMerger.Core.Services;
+
+public class RsdbMergerService
+{
+    public static IRsdbMerger GetMerger(ReadOnlySpan<char> canonical)
+    {
+        return canonical switch {
+            "RSDB/GameSafetySetting.Product.rstbl.byml" => RsdbUniqueRowMergers.NameHash,
+            "RSDB/RumbleCall.Product.rstbl.byml" or
+            "RSDB/UIScreen.Product.rstbl.byml" => RsdbUniqueRowMergers.Name,
+            "RSDB/TagDef.Product.rstbl.byml" => RsdbUniqueRowMergers.FullTagId,
+            "RSDB/ActorInfo.Product.rstbl.byml" or
+            "RSDB/AttachmentActorInfo.Product.rstbl.byml" or
+            "RSDB/Challenge.Product.rstbl.byml" or
+            "RSDB/EnhancementMaterialInfo.Product.rstbl.byml" or
+            "RSDB/EventPlayEnvSetting.Product.rstbl.byml" or
+            "RSDB/EventSetting.Product.rstbl.byml" or
+            "RSDB/GameActorInfo.Product.rstbl.byml" or
+            "RSDB/GameAnalyzedEventInfo.Product.rstbl.byml" or
+            "RSDB/GameEventBaseSetting.Product.rstbl.byml" or
+            "RSDB/GameEventMetadata.Product.rstbl.byml" or
+            "RSDB/LoadingTips.Product.rstbl.byml" or
+            "RSDB/Location.Product.rstbl.byml" or
+            "RSDB/LocatorData.Product.rstbl.byml" or
+            "RSDB/PouchActorInfo.Product.rstbl.byml" or
+            "RSDB/XLinkPropertyTable.Product.rstbl.byml" or
+            "RSDB/XLinkPropertyTableList.Product.rstbl.byml" => RsdbUniqueRowMergers.RowId,
+            "RSDB/Tag.Product.rstbl.byml" => RsdbTagMerger.Shared,
+            _ => throw new ArgumentException($"""
+                No merge is registered for the RSDB file '{canonical}'
+                """)
+        };
+    }
+}
