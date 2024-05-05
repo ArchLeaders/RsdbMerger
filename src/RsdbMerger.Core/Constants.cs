38591103f7fa04860f1d8ac598f214862c9fd821
ArchLeaders <archleadership28@gmail.com>
2024-05-04 17:35:02 -0700

    Create global static constants

---
+++ src/RsdbMerger.Core/Constants.cs
@@ -0,0 +1,11 @@
+﻿global using static RsdbMerger.Core.Constants;
+using Microsoft.IO;
+
+namespace RsdbMerger.Core;
+
+public static class Constants
+{
+    public const string RSDB = "RSDB";
+
+    public static readonly RecyclableMemoryStreamManager MemoryStreamManager = new();
+}
