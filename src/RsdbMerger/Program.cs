a37d0b4e6d31fd179bae81c3aa5a3accb396243b
ArchLeaders <archleadership28@gmail.com>
2024-06-04 16:48:26 -0700

    Update debug runner

--- src/RsdbMerger/Program.cs
+++ src/RsdbMerger/Program.cs
@@ -1,4 +1,10 @@
 ﻿using RsdbMerger.Core.Services;
 
-RsdbChangelogService changelogService = new(args[0], args[1]);
-changelogService.CreateChangelogs();
+for (int i = 0; i < (args.Length - 1); i++) {
+    string arg = args[i];
+    RsdbChangelogService changelogServiceM1 = new(arg, args[i] += ".Changelog");
+    changelogServiceM1.CreateChangelogs();
+}
+
+RsdbMergerService mergerService = new(args.AsSpan()[..^1], args[^1]);
+mergerService.Merge();
